76d943f1a5e131571249aaef6b8fabb179dbe955
Pete Keen <peter.keen@bugsplat.info>
2014-10-01 21:08:37 -0400

    Yet more tests

--- spec/concerns/sellable_spec.rb
+++ spec/concerns/sellable_spec.rb
@@ -3,16 +3,30 @@ require 'spec_helper'
 module Payola
   describe Sellable do
     describe "validations" do
-      it "should validate name"
-      it "should validate permalink"
+      it "should validate" do
+        product = build(:product)
+        expect(product.valid?).to be true
+      end
+      it "should validate name" do
+        product = build(:product, name: nil)
+        expect(product.valid?).to be false
+      end
+      it "should validate permalink" do
+        product = build(:product, permalink: false)
+        expect(product.valid?).to be false
+      end
     end
 
     describe "#product_class" do
-      it "should return the underscore'd version of the class"
+      it "should return the underscore'd version of the class" do
+        expect(build(:product).product_class).to eq 'product'
+      end
     end
 
     describe "#sellable?" do
-      it "should return true"
+      it "should return true" do
+        expect(Product.sellable?).to be true
+      end
     end
   end
 end
--- spec/models/payola/stripe_webhook_spec.rb
+++ spec/models/payola/stripe_webhook_spec.rb
@@ -2,6 +2,16 @@ require 'spec_helper'
 
 module Payola
   describe StripeWebhook do
-    it "should validate the stripe_id"
+    it "should validate" do
+      s = StripeWebhook.new(stripe_id: 'test_id')
+      expect(s.valid?).to be true
+    end
+
+    it "should validate stripe_id" do
+      s = StripeWebhook.create(stripe_id: 'test_id')
+      s2 = StripeWebhook.new(stripe_id: 'test_id')
+
+      expect(s2.valid?).to be false
+    end
   end
 end
