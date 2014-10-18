ef1c13d8912544ebb74070a8c9725a00292049a8
Pete Keen <peter.keen@bugsplat.info>
2014-10-18 10:17:04 -0400

    Add refund handling to template initializer

--- lib/generators/payola/templates/initializer.rb
+++ lib/generators/payola/templates/initializer.rb
@@ -19,4 +19,10 @@ Payola.configure do |config|
   # config.charge_verifier = lambda do |sale|
   #   raise "Nope!" if sale.email.includes?('yahoo.com')
   # end
+
+  # Keep this subscription unless you want to disable refund handling
+  config.subscribe 'charge.refunded' do |event|
+    sale = Payola::Sale.find_by(stripe_id: event.data.object.id)
+    sale.refund!
+  end
 end
