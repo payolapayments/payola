22180a4af0daf83218dde09b1f0d0b87ce8b35b0
Jeremy Green <jeremy@octolabs.com>
2014-11-05 20:59:52 -0600

    Need to use the secret key in the call to Stripe.

--- app/services/payola/create_plan.rb
+++ app/services/payola/create_plan.rb
@@ -1,7 +1,8 @@
 module Payola
   class CreatePlan
     def self.call(subscription_plan)
-      Stripe::Plan.create(
+      secret_key = Payola.secret_key_for_sale(subscription_plan)
+      Stripe::Plan.create({
         :id => subscription_plan.stripe_id,
         :amount => subscription_plan.amount,
         :interval => subscription_plan.interval,
@@ -9,7 +10,7 @@ module Payola
         :currency => subscription_plan.respond_to?(:currency) ? subscription_plan.currency : Payola.default_currency,
         :name => subscription_plan.name,
         :trial_period_days => subscription_plan.trial_period_days
-      )
+      },secret_key)
     end
   end
 end
