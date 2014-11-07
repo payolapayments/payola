ec3af43b5ebb6e474f536b243704e7858576787b
Jeremy Green <jeremy@octolabs.com>
2014-11-06 22:37:26 -0600

    Dealing with subscriptions here, not sales

--- app/assets/javascripts/payola/subscription_form.js
+++ app/assets/javascripts/payola/subscription_form.js
@@ -46,7 +46,7 @@ var PayolaSubscriptionForm = {
         }
         $.get(base_path + '/subscription_status/' + guid, function(data) {
             if (data.status === "finished") {
-                form.append($('<input type="hidden" name="payola_sale_guid"></input>').val(guid));
+                form.append($('<input type="hidden" name="payola_subscription_guid"></input>').val(guid));
                 form.get(0).submit();
             } else if (data.status === "errored") {
                 PayolaSubscriptionForm.showError(form, data.error);
