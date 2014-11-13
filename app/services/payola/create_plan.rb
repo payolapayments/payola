79e7373e406d54aa500ba4ca5bb5ac8b3f565968
Pete Keen <peter.keen@bugsplat.info>
2014-11-13 16:43:44 -0500

    Make trial_period_days actually optional

--- app/services/payola/create_plan.rb
+++ app/services/payola/create_plan.rb
@@ -1,16 +1,17 @@
 module Payola
   class CreatePlan
-    def self.call(subscription_plan)
-      secret_key = Payola.secret_key_for_sale(subscription_plan)
+    def self.call(plan)
+      secret_key = Payola.secret_key_for_sale(plan)
+
       Stripe::Plan.create({
-        :id => subscription_plan.stripe_id,
-        :amount => subscription_plan.amount,
-        :interval => subscription_plan.interval,
-        :interval_count => subscription_plan.interval_count,
-        :currency => subscription_plan.respond_to?(:currency) ? subscription_plan.currency : Payola.default_currency,
-        :name => subscription_plan.name,
-        :trial_period_days => subscription_plan.trial_period_days
-      },secret_key)
+        id:                plan.stripe_id,
+        amount:            plan.amount,
+        interval:          plan.interval,
+        interval_count:    plan.interval_count,
+        currency:          plan.respond_to?(:currency) ? plan.currency : Payola.default_currency,
+        name:              plan.name,
+        trial_period_days: plan.respond_to?(:trial_period_days) ? plan.trial_period_days : nil
+      }, secret_key)
     end
   end
 end
