d6a92039dbb22067d5f3d10ed96448fffe3e5118
Pete Keen <peter.keen@bugsplat.info>
2014-11-17 20:07:28 -0500

    Add simplecov

--- Gemfile
+++ Gemfile
@@ -12,3 +12,4 @@ gemspec
 
 # To use debugger
 # gem 'debugger'
+gem 'simplecov', :require => false, :group => :test
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,12 @@
+require 'simplecov'
+if ENV['CIRCLE_ARTIFACTS']
+
+  dir = File.join("..", "..", "..", ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
+SimpleCov.start 'rails'
+
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment", __FILE__)
