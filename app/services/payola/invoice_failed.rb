3db376690df6793bd60df8a48e20b47019948bc3
Pete Keen <peter.keen@bugsplat.info>
2014-11-17 20:39:53 -0500

    De-dupe code in invoice services

--- app/services/payola/invoice_failed.rb
+++ app/services/payola/invoice_failed.rb
@@ -1,36 +1,11 @@
 module Payola
   class InvoiceFailed
+    include Payola::InvoiceBehavior
+
     def self.call(event)
-      invoice = event.data.object
+      sale, charge = create_sale_from_event(event)
 
-      subscription = Payola::Subscription.find_by!(stripe_id: invoice.subscription)
-      secret_key = Payola.secret_key_for_sale(subscription)
-
-      stripe_sub = Stripe::Customer.retrieve(subscription.stripe_customer_id, secret_key).subscriptions.retrieve(invoice.subscription, secret_key)
-      subscription.sync_with!(stripe_sub)
-
-      sale = Payola::Sale.new do |s|
-        s.email = subscription.email
-        s.state = 'processing'
-        s.owner = subscription
-        s.product = subscription.plan
-        s.stripe_token = 'invoice'
-        s.amount = invoice.total
-        s.currency = invoice.currency
-      end
-
-      charge = Stripe::Charge.retrieve(invoice.charge, secret_key)
-
-      sale.stripe_id  = charge.id
-      sale.card_type  = charge.card.respond_to?(:brand) ? charge.card.brand : charge.card.type
-      sale.card_last4 = charge.card.last4
-
-      if charge.respond_to?(:fee)
-        sale.fee_amount = charge.fee
-      else
-        balance = Stripe::BalanceTransaction.retrieve(charge.balance_transaction, secret_key)
-        sale.fee_amount = balance.fee
-      end
+      return unless sale
 
       sale.error = charge.failure_message
       sale.save!
