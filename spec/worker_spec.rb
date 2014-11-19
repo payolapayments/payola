6ae9434fa84f06887af9ebe6722d847618ba5700
Pete Keen <peter.keen@bugsplat.info>
2014-11-18 19:56:42 -0500

    Add specs for sucker punch worker

--- spec/worker_spec.rb
+++ spec/worker_spec.rb
@@ -32,6 +32,24 @@ module Payola
     end
   end
 
+  describe Worker::SuckerPunch do
+    describe "#can_run?" do
+      it "should return true if SuckerPunch is defined" do
+        expect(Payola::Worker::SuckerPunch.can_run?).to be_truthy
+      end
+    end
+
+    describe "#call" do
+      it "should call async" do
+        worker = double
+        expect(Payola::Worker::SuckerPunch).to receive(:new).and_return(worker)
+        expect(worker).to receive(:async).and_return(worker)
+        expect(worker).to receive(:perform)
+        Payola::Worker::SuckerPunch.call(Payola::TestService, double)
+      end
+    end
+  end
+
   describe Worker::ActiveJob do
     before do
       module ::ActiveJob
