a4b671a881bd7d500cc41a6269144daabfc1ebd7
Trevor Hinesley <trevor.hinesley@gmail.com>
2014-12-15 19:01:33 -0600

    Added Payola::ChangeSubscriptionQuantity, similar to Payola::ChangeSubscriptionPlan

---
+++ app/services/payola/change_subscription_quantity.rb
@@ -0,0 +1,25 @@
+module Payola
+  class ChangeSubscriptionQuantity
+    def self.call(subscription, quantity)
+      secret_key = Payola.secret_key_for_sale(subscription)
+      old_quantity = subscription.quantity
+
+      begin
+        customer = Stripe::Customer.retrieve(subscription.stripe_customer_id, secret_key)
+        sub = customer.subscriptions.retrieve(subscription.stripe_id)
+        sub.quantity = quantity
+        sub.save
+
+        subscription.quantity = quantity
+        subscription.save!
+
+        subscription.instrument_plan_changed(old_quantity)
+
+      rescue RuntimeError, Stripe::StripeError => e
+        subscription.errors[:base] << e.message
+      end
+
+      subscription
+    end
+  end
+end
