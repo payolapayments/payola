payolapayments/payola

commit e739e1678553c31433f87ee5087db5afd70acb46

Attempt to fix the ENV load order problem
Lots of users use gems like Dotenv or Figaro to handle their ENV
setup. The problem is that sometimes these gems get loaded after
Payola, which means the secret and publishable keys end up nil.
This patch changes the default secret and publishable keys into
lambdas that get called at the appropriate time, which should
fix the problem.

lib/payola.rb
@@ -75,10 +75,10 @@ module Payola
       self.background_worker = nil
       self.event_filter = lambda { |event| event }
       self.charge_verifier = lambda { |event| true }
-      self.publishable_key = ENV['STRIPE_PUBLISHABLE_KEY']
-      self.secret_key = ENV['STRIPE_SECRET_KEY']
-      self.secret_key_retriever = lambda { |sale| Payola.secret_key }
-      self.publishable_key_retriever = lambda { |sale| Payola.publishable_key }
+      self.publishable_key = lambda { ENV['STRIPE_PUBLISHABLE_KEY'] }
+      self.secret_key = lambda { ENV['STRIPE_SECRET_KEY'] }
+      self.secret_key_retriever = lambda { |sale| Payola.secret_key.respond_to?(:call) ? Payola.secret_key.call() : Payola.secret_key  }
+      self.publishable_key_retriever = lambda { |sale| Payola.publishable_key.respond_to?(:call) ? Payola.publishable_key.call() : Payola.publishable_key }
       self.support_email = 'sales@example.com'
       self.default_currency = 'usd'
       self.sellables = {}

spec/payola_spec.rb
@@ -13,13 +13,13 @@ module Payola
     it "should set publishable key from env" do
       ENV['STRIPE_PUBLISHABLE_KEY'] = 'some_key'
       Payola.reset!
-      expect(Payola.publishable_key).to eq 'some_key'
+      expect(Payola.publishable_key.call).to eq 'some_key'
     end
 
     it "should set secret key from env" do
       ENV['STRIPE_SECRET_KEY'] = 'some_secret'
       Payola.reset!
-      expect(Payola.secret_key).to eq 'some_secret'
+      expect(Payola.secret_key.call).to eq 'some_secret'
     end
   end
 

commit 828403160278f07a5e5b7fe16d0230cc6673f775

Don't require callers to #call the keys
e739e16 introduced a regression where the Payola user has to `#call`
the result of the public and private keys. This commit instead wraps
the env in a wrapper object that properly evaluates `to_s` and `==`.

Fixes #71

lib/payola.rb
@@ -75,10 +75,10 @@ module Payola
       self.background_worker = nil
       self.event_filter = lambda { |event| event }
       self.charge_verifier = lambda { |event| true }
-      self.publishable_key = lambda { ENV['STRIPE_PUBLISHABLE_KEY'] }
-      self.secret_key = lambda { ENV['STRIPE_SECRET_KEY'] }
-      self.secret_key_retriever = lambda { |sale| Payola.secret_key.respond_to?(:call) ? Payola.secret_key.call() : Payola.secret_key  }
-      self.publishable_key_retriever = lambda { |sale| Payola.publishable_key.respond_to?(:call) ? Payola.publishable_key.call() : Payola.publishable_key }
+      self.publishable_key = EnvWrapper.new('STRIPE_PUBLISHABLE_KEY')
+      self.secret_key = EnvWrapper.new('STRIPE_SECRET_KEY')
+      self.secret_key_retriever = lambda { |sale| Payola.secret_key  }
+      self.publishable_key_retriever = lambda { |sale| Payola.publishable_key }
       self.support_email = 'sales@example.com'
       self.default_currency = 'usd'
       self.sellables = {}
@@ -118,6 +118,20 @@ module Payola
       StripeWebhook.create!(stripe_id: params[:id])
       event = Stripe::Event.retrieve(params[:id], Payola.secret_key)
       Payola.event_filter.call(event)
+    end
+  end
+
+  class EnvWrapper
+    def initialize(key)
+      @key = key
+    end
+
+    def to_s
+      ENV[@key]
+    end
+
+    def ==(other)
+      to_s == other.to_s
     end
   end
 

spec/payola_spec.rb
@@ -13,13 +13,13 @@ module Payola
     it "should set publishable key from env" do
       ENV['STRIPE_PUBLISHABLE_KEY'] = 'some_key'
       Payola.reset!
-      expect(Payola.publishable_key.call).to eq 'some_key'
+      expect(Payola.publishable_key).to eq 'some_key'
     end
 
     it "should set secret key from env" do
       ENV['STRIPE_SECRET_KEY'] = 'some_secret'
       Payola.reset!
-      expect(Payola.secret_key.call).to eq 'some_secret'
+      expect(Payola.secret_key).to eq 'some_secret'
     end
   end
 