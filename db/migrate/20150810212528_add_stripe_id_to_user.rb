efb9b7cabb2515ddb6e2e808e8b44f1550ab2521
jakedavies <jgdavies24@gmail.com>
2015-09-13 12:52:27 -0700

    move stripe_id to dummy migration

--- db/migrate/20150810212528_add_stripe_id_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddStripeIdToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :stripe_id, :string
-  end
-end
--- spec/dummy/db/migrate/20141204170622_create_users.rb
+++ spec/dummy/db/migrate/20141204170622_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string :stripe_id
       t.timestamps
     end
   end
