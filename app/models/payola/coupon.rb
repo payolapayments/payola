658ad11834c706a05da172de0ee2f1e9dac9e566
Dave Kiss <dave@davekiss.com>
2016-04-29 14:47:44 -0400

    Destroy stripe coupons before destroying locally

--- app/models/payola/coupon.rb
+++ app/models/payola/coupon.rb
@@ -4,9 +4,14 @@ module Payola
     validates_presence_of :duration
 
     before_create  :create_stripe_coupon,  if: -> { Payola.create_stripe_coupons }
+    before_destroy :destroy_stripe_coupon, if: -> { Payola.create_stripe_coupons }
 
     def create_stripe_coupon
       Payola::CreateCoupon.call(self)
+    end
+
+    def destroy_stripe_coupon
+      Payola::DestroyCoupon.call(self.code)
     end
   end
 end
