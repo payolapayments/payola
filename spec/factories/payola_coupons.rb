c1299fd73b2860c6c6dda2f2f78a8b508ba1e960
Dave Kiss <dave@davekiss.com>
2016-04-29 14:35:47 -0400

    Add new fields to coupon factory

--- spec/factories/payola_coupons.rb
+++ spec/factories/payola_coupons.rb
@@ -4,5 +4,28 @@ FactoryGirl.define do
   factory :payola_coupon, :class => 'Payola::Coupon' do
     code "MyString"
     percent_off 1
+
+    duration 'once'
+    max_redemptions 1
+    redeem_by 1.day.from_now
+
+    trait :flat_amount do
+      percent_off nil
+      amount_off 2
+      currency 'USD'
+    end
+
+    trait :repeating do
+      duration 'repeating'
+      duration_in_months 1
+    end
+
+    trait :perpetual do
+      duration 'forever'
+    end
+
+    trait :expired do
+      redeem_by 1.day.ago
+    end
   end
 end
