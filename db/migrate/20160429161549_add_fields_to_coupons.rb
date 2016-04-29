ee5820a2e42f4c23c2134181d59eab32dcc51b33
Dave Kiss <dave@davekiss.com>
2016-04-29 16:52:31 -0400

    Remove unused default for amount off

--- db/migrate/20160429161549_add_fields_to_coupons.rb
+++ db/migrate/20160429161549_add_fields_to_coupons.rb
@@ -1,6 +1,6 @@
 class AddFieldsToCoupons < ActiveRecord::Migration
   def change
-    add_column :payola_coupons, :amount_off, :integer, default: true
+    add_column :payola_coupons, :amount_off, :integer
     add_column :payola_coupons, :duration, :string, default: 'once'
     add_column :payola_coupons, :duration_in_months, :integer
     add_column :payola_coupons, :max_redemptions, :integer
