69668b0058d0efb629007ec3f071e6d684457358
Jeremiah <Tongboy@users.noreply.github.com>
2017-06-11 11:43:25 -0700

    Create sale.rb

--- app/models/payola/sale.rb
+++ app/models/payola/sale.rb
@@ -12,10 +12,10 @@ module Payola
     validates_presence_of :stripe_token
     validates_presence_of :currency
 
-    belongs_to :product, polymorphic: true
-    belongs_to :owner, polymorphic: true, optional: true
-    belongs_to :coupon, optional: true
-    belongs_to :affiliate, optional: true
+    belongs_to :product, Rails::VERSION::MAJOR > 4 ? { polymorphic: true, optional: true } : { polymorphic: true }
+    belongs_to :owner, Rails::VERSION::MAJOR > 4 ? { polymorphic: true, optional: true } : { polymorphic: true }
+    belongs_to :coupon, Rails::VERSION::MAJOR > 4 ? { optional: true } : {}
+    belongs_to :affiliate, Rails::VERSION::MAJOR > 4 ? { optional: true } : {}
 
     include AASM
 
